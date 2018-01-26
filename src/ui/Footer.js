150df15cb1990571b46b6c5137bef6a1fe6548c5
TensaZangetsu <l.lawliet46@gmail.com>
2018-01-26 10:53:25 +0100

    Ispravka

--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 
-export const Footer = () => ( <
-    footer className = "footer" >
-    &
-    copy; 2018. Milos Radoman.Developed as test
-    for quantox technolgies. <
-    /footer>
-);
+export const Footer = () => ( <footer class="footer">
+&copy; 2018. Milos Radoman. Developed as test for quantox technolgies.
+</footer>);
